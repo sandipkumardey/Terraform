b7523e3e47669dc2184a43dd80e51529548101d9
Sandip Kumar Dey <96774612+sandipkumardey@users.noreply.github.com>
2025-01-05 20:00:02 +0530

    added challenge 3: creating a Github repo with TF

--- Terraform Course/main.tf
+++ Terraform Course/main.tf
@@ -8,11 +8,6 @@
 #always keep your access key and secret access key in key in aws configuure command
 
 
-provider "aws" {
-  # Configuration options
-  region = "us-east-1"
-}
-
 resource "aws_instance" "web" {
   ami           = "ami-0e2c8caa4b6378d8c"  #ubuntu
   instance_type = "t2.micro"
@@ -20,4 +15,12 @@ resource "aws_instance" "web" {
   tags = {
     Name = "HelloWorld"
   }
+}
+
+
+resource "github_repository" "example" {
+  name        = "My_awesome_github_repo"
+  description = "This repo is created using Terraform"
+
+  visibility = "public"
 }
